be9fd37db326e0db2efbd09f744b363f4f257529
sjlewis1996 <154299089+sjlewis1996@users.noreply.github.com>
2026-05-16 01:48:13 -0400

    Update aliases.sh

--- setup/aliases.sh
+++ setup/aliases.sh
@@ -1,5 +1,5 @@
 # UAS Lab Aliases
 
-alias simplane="~/Downloads/QGroundControl-x86_64.AppImage & cd ~/ardupilot/ArduPlane && python3 ~/uas-lab/scripts/flight_log_summary.py & cd ~/ardupilot/ArduPlane && python3 ../Tools/autotest/sim_vehicle.py -v ArduPlane --out=udp:127.0.0.1:14551"
+alias simplane="~/Downloads/QGroundControl-x86_64.AppImage & cd ~/ardupilot/ArduPlane && python3 ~/uas-lab/scripts/flight_log_summary.py & cd ~/ardupilot/ArduPlane && python3 ../Tools/autotest/sim_vehicle.py -v ArduPlane -L KLZU_RWY7 --out=udp:127.0.0.1:14551"
 
 alias flightlog="cd ~/uas-lab/scripts && python3 flight_log_summary.py"
