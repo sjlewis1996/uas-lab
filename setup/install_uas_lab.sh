d78e1fd13bfb42c770f08a34948c3aea016f78bb
sjlewis1996 <154299089+sjlewis1996@users.noreply.github.com>
2026-05-16 00:46:09 -0400

    Update install_uas_lab.sh

--- setup/install_uas_lab.sh
+++ setup/install_uas_lab.sh
@@ -4,6 +4,9 @@ sudo apt update && sudo apt upgrade -y
 echo "Installing required packages..."
 sudo apt install git wget curl python3 python3-pip python3-venv python3-dev python3-matplotlib python3-pandas libopengl0 libgl1 libegl1 mesa-utils -y
 
+echo "Removing ModemManager..."
+sudo apt remove modemmanager -y
+
 echo "Installing Python MAVLink tools..."
 python3 -m pip install --break-system-packages --force-reinstall empy==3.3.4 pymavlink MAVProxy future
 
